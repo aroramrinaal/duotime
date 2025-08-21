0ee861896a83872af29ed188427b6714482def0b
aroramrinaal <aroramrinaal@gmail.com>
2025-08-21 16:31:54 +0530

    Initial Commit

---
+++ duotime/duotimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  duotimeApp.swift
+//  duotime
+//
+//  Created by Mrinaal Arora on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct duotimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ duotimeTests/duotimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  duotimeTests.swift
+//  duotimeTests
+//
+//  Created by Mrinaal Arora on 8/21/25.
+//
+
+import Testing
+@testable import duotime
+
+struct duotimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
